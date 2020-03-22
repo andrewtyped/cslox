b6cf1f252b403a3186866864af025dc5c77855b6
Andrew Barger <asbarg01@gmail.com>
2020-03-22 15:51:29 -0400

    Initial commit of token class for storing units of Lox syntax

---
+++ src/lox/Token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lox
+{
+    /// <summary>
+    /// Represents a unit of Lox syntax
+    /// </summary>
+    public class Token
+    {
+    }
+}
---
+++ test/lox.test/TokenTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lox.test
+{
+    [TestClass]
+    public class TokenTest
+    {
+        [TestMethod]
+        public void CanConstructToken()
+        {
+            var token = new Token();
+        }
+    }
+}
