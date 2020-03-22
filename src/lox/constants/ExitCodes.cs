b8869fae5b9bef6e9e233b864f5a66cf15917aae
Andrew Barger <asbarg01@gmail.com>
2020-03-22 14:48:54 -0400

    Convert exit codes enum to constants so we can return the const directly from Main
    We cannot return an enum from Main, nor can we implicitly convert an enum to an
    int.

--- src/lox/constants/ExitCodes.cs
+++ src/lox/constants/ExitCodes.cs
@@ -4,11 +4,16 @@
     /// Program exit codes defined by sysexits.h from the FreeBSD manual:
     /// https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
     /// </summary>
-    public enum ExitCodes
+    public static class ExitCodes
     {
+        /// <summary>
+        /// The command executed successfully.
+        /// </summary>
+        public const int EX_OK = 0;
+
         /// <summary>
         /// The command was used incorrectly, e.g., with the wrong number of arguments, a bad flag, a bad syntax in a parameter, etc.
         /// </summary>
-        EX_USAGE = 64
+        public const int EX_USAGE = 64;
     }
 }
