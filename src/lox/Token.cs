31c1b6e43373b5ec309fff479ca3adebf45623a9
Andrew Barger <asbarg01@gmail.com>
2020-03-22 20:05:15 -0400

    Define model for tokens

--- src/lox/Token.cs
+++ src/lox/Token.cs
@@ -1,13 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using lox.constants;
 
 namespace lox
 {
     /// <summary>
-    /// Represents a unit of Lox syntax
+    /// Represents a unit of Lox syntax.
     /// </summary>
-    public class Token
+    public sealed class Token
     {
+        #region Constructors
+
+        /// <summary>
+        /// Create a non-literal token.
+        /// </summary>
+        /// <param name="lexeme">The text of the token.</param>
+        /// <param name="line">The line on which the token appears.</param>
+        /// <param name="type">The type of token represented by the lexeme.</param>
+        public Token(string lexeme,
+                     int line,
+                     TokenType type)
+        {
+            this.Lexeme = lexeme;
+            this.Line = line;
+            this.Type = type;
+        }
+
+        /// <summary>
+        /// Create a literal token.
+        /// </summary>
+        /// <param name="lexeme">The text of the token.</param>
+        /// <param name="line">The line on which the token appears.</param>
+        /// <param name="literal">The literal value of the token (string, number, identifier).</param>
+        /// <param name="type">The type of token represented by the lexeme.</param>
+        public Token(string lexeme,
+                     int line,
+                     object literal,
+                     TokenType type)
+        {
+            this.Lexeme = lexeme;
+            this.Line = line;
+            this.Literal = literal;
+            this.Type = type;
+        }
+
+        #endregion
+
+        #region Instance Properties
+
+        /// <summary>
+        /// Get the text of the token as it appeared in source code.
+        /// </summary>
+        public string Lexeme
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Get the line number in source code where the token appeared.
+        /// </summary>
+        public int Line
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Get the literal value of the token, if any. Null if the token type is not Number or String.
+        /// </summary>
+        public object? Literal
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Get the type code of the token.
+        /// </summary>
+        public TokenType Type
+        {
+            get;
+        }
+
+        #endregion
     }
 }
--- test/lox.test/TokenTest.cs
+++ test/lox.test/TokenTest.cs
@@ -1,17 +1,47 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
+
+using static lox.constants.TokenType;
 
 namespace lox.test
 {
     [TestClass]
     public class TokenTest
     {
+        #region Instance Methods
+
         [TestMethod]
-        public void CanConstructToken()
+        public void CanConstructNonLiteralToken()
         {
-            var token = new Token();
+            var token = new Token("for",
+                                  1,
+                                  FOR);
+
+            Assert.AreEqual("for",
+                            token.Lexeme);
+            Assert.AreEqual(1,
+                            token.Line);
+            Assert.AreEqual(FOR,
+                            token.Type);
         }
+
+        [TestMethod]
+        public void CanConstructLiteralToken()
+        {
+            var token = new Token("My string",
+                                  2,
+                                  "My string",
+                                  STRING);
+
+            Assert.AreEqual("My string",
+                            token.Lexeme);
+            Assert.AreEqual(2,
+                            token.Line);
+            Assert.AreEqual("My string",
+                            token.Literal);
+            Assert.AreEqual(STRING,
+                            token.Type);
+        }
+
+        #endregion
     }
 }
