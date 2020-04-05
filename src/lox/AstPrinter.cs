3692848492e62604608aa24b01fc5b631c82e40d
Andrew Barger <asbarg01@gmail.com>
2020-04-05 16:10:53 -0400

    Implement ASTPrinter to visualize lox abstract syntax trees

---
+++ src/lox/AstPrinter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+
+namespace lox
+{
+    /// <summary>
+    /// Expression visitor capable of rendering a lox AST as a string which illustrates the tree's structure.
+    /// </summary>
+    public class AstPrinter : Expr.IVisitor<string>
+    {
+        #region Instance Methods
+
+        public string Print(Expr expr,
+                            in ReadOnlySpan<char> source)
+        {
+            return expr.Accept(this,
+                               source);
+        }
+
+        public string VisitBinaryExpr(Expr.Binary expr,
+                                      in ReadOnlySpan<char> source)
+        {
+            return this.Parenthesize(expr.op.GetLexeme(source),
+                                     source,
+                                     expr.left,
+                                     expr.right);
+        }
+
+        public string VisitGroupingExpr(Expr.Grouping expr,
+                                        in ReadOnlySpan<char> source)
+        {
+            return this.Parenthesize("group",
+                                     source,
+                                     expr.expression);
+        }
+
+        public string VisitLiteralExpr(Expr.Literal expr,
+                                       in ReadOnlySpan<char> source)
+        {
+            return expr.value?.ToString() ?? "nil";
+        }
+
+        public string VisitUnaryExpr(Expr.Unary expr,
+                                     in ReadOnlySpan<char> source)
+        {
+            return this.Parenthesize(expr.op.GetLexeme(source),
+                                     source,
+                                     expr.right);
+        }
+
+        private string Parenthesize(in ReadOnlySpan<char> name,
+                                    in ReadOnlySpan<char> source,
+                                    params Expr[] exprs)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append('(')
+              .Append(name);
+
+            for (int i = 0;
+                 i < exprs.Length;
+                 i++)
+            {
+                sb.Append(' ')
+                  .Append(exprs[i]
+                              .Accept(this,
+                                      source));
+            }
+
+            sb.Append(')');
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ test/lox.test/AstPrinterTest.cs
@@ -0,0 +1,102 @@
+﻿using lox.constants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static lox.Expr;
+
+namespace lox.test
+{
+    [TestClass]
+    public class AstPrinterTest
+    {
+        private AstPrinter astPrinter = new AstPrinter();
+        
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.astPrinter = new AstPrinter();
+        }
+
+        [DataTestMethod]
+        [DataRow(1, "1")]
+        [DataRow("hello world","hello world")]
+        [DataRow(null, "nil")]
+        public void CanPrintLiteralExpr(object? literalValue, string expectedPrint)
+        {
+            var literal = new Literal(literalValue);
+
+            this.AssertPrint(literal,
+                             expectedPrint,
+                             expectedPrint);
+        }
+
+        [TestMethod]
+        public void CanPrintGroupingExpr()
+        {
+            var group = new Grouping(new Literal(1));
+
+            this.AssertPrint(group,
+                             "(1)",
+                             "(group 1)");
+        }
+
+        [TestMethod]
+        public void CanPrintBinaryExpr()
+        {
+            var source = "3 + 1";
+            var binary = new Binary(new Literal(3),
+                                    new Token(2,
+                                              3,
+                                              1,
+                                              TokenType.PLUS),
+                                    new Literal(1));
+
+            this.AssertPrint(binary,
+                             source,
+                             "(+ 3 1)");
+        }
+
+        [TestMethod]
+        public void CanPrintUnaryExpression()
+        {
+            var source = "-1";
+            var unary = new Unary(new Token(0,
+                                            1,
+                                            1,
+                                            TokenType.MINUS),
+                                  new Literal(1));
+            this.AssertPrint(unary,
+                             source,
+                             "(- 1)");
+        }
+
+        [TestMethod]
+        public void CanPrintDeepExpression()
+        {
+            var source = "-123 * (45.67)";
+            var expr = new Binary(new Unary(new Token(0,
+                                                      1,
+                                                      1,
+                                                      TokenType.MINUS),
+                                            new Literal(123)),
+                                  new Token(5,
+                                            6,
+                                            1,
+                                            TokenType.STAR),
+                                  new Grouping(new Literal(45.67)));
+
+            this.AssertPrint(expr,
+                             source,
+                             "(* (- 123) (group 45.67))");
+        }
+
+        private void AssertPrint(Expr expr,string source, string expectedPrint)
+        {
+            var actualPrint = this.astPrinter!.Print(expr,
+                                                     source);
+            Assert.AreEqual(expectedPrint,
+                            actualPrint);
+        }
+    }
+}
