a6a71d016362b8759581d5970c750b28185c2f8d
Andrew Barger <abarger@bastiansolutions.com>
2020-04-25 14:46:55 -0400

    Add a utility type to store both scanned tokens and their source
    This makes lexeme retrieval easy and also saves us from needing to pass
    TWO extra parameters (tokens and source) through all the parser methods.

---
+++ src/lox/ScannedSource.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace lox
+{
+    /// <summary>
+    /// Stores scanned tokens alongside their source string for easy passage through
+    /// the parser and easy lexeme retrieval.
+    /// </summary>
+    public ref struct ScannedSource
+    {
+        #region Constructors
+
+        public ScannedSource(ReadOnlySpan<Token> tokens,
+                             ReadOnlySpan<char> source)
+        {
+            this.Tokens = tokens;
+            this.Source = source;
+        }
+
+        #endregion
+
+        #region Instance Properties
+
+        /// <summary>
+        /// Get the fully sscanned source.
+        /// </summary>
+        public ReadOnlySpan<char> Source
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Get the tokens scanned from the source.
+        /// </summary>
+        public ReadOnlySpan<Token> Tokens
+        {
+            get;
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        /// <summary>
+        /// Gets the lexeme for a token in <see cref="Tokens"/> from <see cref="Source"/>.
+        /// </summary>
+        /// <param name="token">The token to retrieve the lexeme for.</param>
+        /// <returns>The lexeme representing <paramref name="token"/></returns>
+        public ReadOnlySpan<char> GetLexeme(in Token token)
+        {
+            return token.GetLexeme(this.Source);
+        }
+
+        #endregion
+    }
+}
