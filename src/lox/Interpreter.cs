ddc3246a298763bfea73ca93080c6cef78fafb4d
Andrew Barger <abarger@bastiansolutions.com>
2020-04-26 20:21:58 -0400

    Scaffold interpreter, start interpreting literal expressions

---
+++ src/lox/Interpreter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lox
+{
+    /// <summary>
+    /// Evaluates a Lox AST and produces its value.
+    /// </summary>
+    public class Interpreter : Expr.IVisitor<object?>
+    {
+        #region Instance Methods
+
+        public object? VisitBinaryExpr(Expr.Binary expr,
+                                      in ReadOnlySpan<char> source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object? VisitGroupingExpr(Expr.Grouping expr,
+                                        in ReadOnlySpan<char> source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object? VisitLiteralExpr(Expr.Literal expr,
+                                       in ReadOnlySpan<char> source)
+        {
+            return expr.value;
+        }
+
+        public object? VisitUnaryExpr(Expr.Unary expr,
+                                     in ReadOnlySpan<char> source)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ test/lox.test/interpreter/InterpreterTestBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lox.test.interpreter
+{
+    [TestClass]
+    public class InterpreterTestBase
+    {
+        #region Fields
+
+        protected Interpreter Interpreter = new Interpreter();
+
+        #endregion
+
+        #region Instance Methods
+
+        protected object? Interpret(string source)
+        {
+            var expr = this.Parse(source);
+            var value = expr.Accept(this.Interpreter,
+                                    source);
+
+            return value;
+        }
+
+        protected Expr Parse(string source)
+        {
+            var scanner = new Scanner();
+            var parser = new Parser();
+
+            var scannedTokens = scanner.ScanTokens(source);
+            var expr = parser.Parse(new ScannedSource(scannedTokens.ToArray(),
+                                                      source));
+
+            return expr;
+        }
+
+        #endregion
+    }
+
+    [TestClass]
+    public class LiteralInterpretationTest : InterpreterTestBase
+    {
+        #region Instance Methods
+
+        [DataTestMethod]
+        [DataRow("\"Test\"", "Test")]
+        [DataRow("1", 1d)]
+        [DataRow("nil", null)]
+        public void CanInterpretLiterals(string source, object expectedValue)
+        {
+            var value = this.Interpret(source);
+            Assert.AreEqual(expectedValue,
+                            value);
+        }
+
+        #endregion
+    }
+}
