c12026c5038ffdf118b78909ce1afaa45ae9e739
Andrew Barger <asbarg01@gmail.com>
2020-05-02 14:28:24 -0400

    Add shim for Java's void type for our statement visitor

---
+++ src/lox/Void.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lox
+{
+    /// <summary>
+    /// Represents an empty return type since C# doesn't have a built-in void type like Java.
+    /// </summary>
+    public struct Void
+    {
+    }
+}
