6ed317aaf52714d21a1555d2177bfc8e5f596433
Andrew Barger <asbarg01@gmail.com>
2020-05-13 17:45:17 -0400

    Add tests for parsing logical operators - not passing yet.

--- test/lox.test/parser/LogicalExprTest.cs
+++ test/lox.test/parser/LogicalExprTest.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using lox.constants;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static lox.Expr;
 
 namespace lox.test.parser
 {
     [TestClass]
     public class LogicalExprTest : ParserTestBase
     {
+        #region Instance Methods
 
+        [TestMethod]
+        public void CanParseLogicalAndExpression()
+        {
+            var source = "true and 1 == 1";
+            var expr = this.AssertExpr<Logical>(source);
+
+            this.AssertExpr<Literal>(expr.left);
+            this.AssertExpr<Binary>(expr.right);
+            Assert.AreEqual(TokenType.AND,
+                            expr.op.Type);
+        }
+
+        [TestMethod]
+        public void CanParseLogicalOrExpression()
+        {
+            var source = "true or false";
+            var expr = this.AssertExpr<Logical>(source);
+
+            this.AssertExpr<Literal>(expr.left);
+            this.AssertExpr<Literal>(expr.right);
+            Assert.AreEqual(TokenType.OR,
+                            expr.op.Type);
+        }
+
+        #endregion
     }
 }
