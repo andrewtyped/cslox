bacd3f8badb4d46ee9589d5bc6b158d94d0ef0f9
Andrew Barger <asbarg01@gmail.com>
2020-05-27 20:15:18 -0400

    Define native clock function

---
+++ src/lox/loxFunctions/Clock.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lox.loxFunctions
+{
+    /// <summary>
+    /// Native lox function returning the elapsed time since the Unix epoch in seconds.
+    /// </summary>
+    public class Clock : ILoxCallable
+    {
+        private readonly IDateTimeOffsetProvider dateTimeOffsetProvider;
+
+        public Clock(IDateTimeOffsetProvider dateTimeOffsetProvider)
+        {
+            this.dateTimeOffsetProvider = dateTimeOffsetProvider ?? throw new ArgumentNullException(nameof(dateTimeOffsetProvider));
+        }
+
+        #region Instance Methods
+
+        public override string ToString() => "<native fn>";
+
+        public int Arity() => 0;
+
+        public object? Call(Interpreter interpreter,
+                            List<object?> arguments,
+                            in ReadOnlySpan<char> source)
+        {
+            return this.dateTimeOffsetProvider.UtcNow()
+                       .ToUnixTimeMilliseconds()
+                   / 1000.0d;
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Exposes operations for getting the current time.
+    /// </summary>
+    public interface IDateTimeOffsetProvider
+    {
+        #region Instance Methods
+
+        /// <summary>
+        /// Gets the current time in UTC.
+        /// </summary>
+        DateTimeOffset UtcNow();
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Default implementation of <see cref="IDateTimeOffsetProvider"/>
+    /// which utilitizes <see cref="System.DateTimeOffset"/>.
+    /// </summary>
+    public class DateTimeOffsetProvider : IDateTimeOffsetProvider
+    {
+        #region Instance Methods
+
+        public DateTimeOffset UtcNow() => DateTimeOffset.UtcNow;
+
+        #endregion
+    }
+}
---
+++ test/lox.test/loxFunctions/ClockTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+using lox.loxFunctions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lox.test.loxFunctions
+{
+    [TestClass]
+    public class ClockTest
+    {
+        #region Fields
+
+        private readonly Clock clock;
+
+        private readonly MockDateTimeProvider dateTimeProvider;
+
+        #endregion
+
+        #region Constructors
+
+        public ClockTest()
+        {
+            this.dateTimeProvider = new MockDateTimeProvider();
+            this.clock = new Clock(this.dateTimeProvider);
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        [TestMethod]
+        public void ClockHasArity0()
+        {
+            Assert.AreEqual(0,
+                            this.clock.Arity());
+        }
+
+        [TestMethod]
+        public void ClockReturnsSecondsSinceUnixEpoch()
+        {
+            var dayAfterUnixEpoch = new DateTimeOffset(1970,
+                                                       1,
+                                                       2,
+                                                       0,
+                                                       0,
+                                                       0,
+                                                       default);
+            this.dateTimeProvider.MockUtcNow = dayAfterUnixEpoch;
+
+            const double secondsPerDay = 86400.0d;
+
+            var currentTime = this.clock.Call(new Interpreter(),
+                                              new List<object?>(),
+                                              new ReadOnlySpan<char>());
+
+            Assert.AreEqual(secondsPerDay,
+                            currentTime);
+        }
+
+        #endregion
+    }
+
+    public class MockDateTimeProvider : IDateTimeOffsetProvider
+    {
+        #region Fields
+
+        public DateTimeOffset MockUtcNow;
+
+        #endregion
+
+        #region Instance Methods
+
+        public DateTimeOffset UtcNow()
+        {
+            return this.MockUtcNow;
+        }
+
+        #endregion
+    }
+}
