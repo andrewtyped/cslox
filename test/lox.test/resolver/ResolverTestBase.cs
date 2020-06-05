fcfdb707c0767abf2daf8a60b152d870fb9864d8
Andrew Barger <abarger@bastiansolutions.com>
2020-06-04 20:50:40 -0400

    Do static resolution pass to place variables in correct scope

---
+++ test/lox.test/resolver/ResolverTestBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lox.test.resolver
+{
+    [TestClass]
+    public class ResolverTestBase
+    {
+        protected Resolver resolver;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.resolver = new Resolver(new Interpreter(new Mock<IConsole>().Object));
+        }
+
+        protected void Resolve(string source)
+        {
+            var scanner = new Scanner();
+            var tokens = scanner.ScanTokens(source);
+            var parser = new Parser();
+            var statements = parser.Parse(new ScannedSource(new ReadOnlySpan<Token>(tokens.ToArray()),
+                                                            source));
+
+            if (parser.ParseErrors.Any())
+            {
+                string joinedErrors = string.Join(System.Environment.NewLine,
+                                                  parser.ParseErrors.Select(err => err.Message));
+                Assert.Fail(joinedErrors);
+            }
+
+            this.resolver.Resolve(statements,
+                                  source);
+        }
+    }
+}
